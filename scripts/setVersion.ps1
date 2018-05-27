4d8ab1c26bd604e5c28012e36d6fa17fa75754fc
Noel Bundick <noelbundick@gmail.com>
2018-05-27 15:05:10 -0700

    Set version in Release instead of Build->Release

--- scripts/setVersion.ps1
+++
@@ -1,2 +0,0 @@
-$version = (Select-String .\src\noelbundick\setup.py -Pattern "^VERSION = ""(.*)""").Matches.Groups[1].Value
-Write-Host "##vso[task.setvariable variable=extensionVersion;isSecret=false;isOutput=true;]$version"
