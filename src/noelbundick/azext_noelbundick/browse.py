a025fa2a47e8cdbec839e6a925d82815b6ee023a
Noel Bundick <noelbundick@gmail.com>
2019-05-22 15:43:11 -0700

    Extract azbrowse correctly on Windows

--- src/noelbundick/azext_noelbundick/browse.py
+++ src/noelbundick/azext_noelbundick/browse.py
@@ -1,16 +1,18 @@
 import json
 import operator
 import os
+import platform
 import re
 import requests
 import shutil
 import subprocess
-import sys
 import tarfile
 
 from azure.cli.core.api import get_config_dir
 from azure.cli.core.commands import CliCommandType
+from io import BytesIO
 from knack.log import get_logger
+from zipfile import ZipFile
 
 logger = get_logger(__name__)
 
@@ -26,28 +28,40 @@ def load_arguments(self, _):
     pass
 
 
-def get_latest_azbrowse():
-    platform = sys.platform
+def get_latest_azbrowse(current_platform):
+    target_platform = '{}_amd64'.format(current_platform.lower())
 
     r = requests.get('https://api.github.com/repos/lawrencegripper/azbrowse/releases')
     releases = r.json()
 
     releases = sorted(releases, key=lambda x: x['published_at'], reverse=True)
     latest = (x['browser_download_url'] for x in releases[0]['assets'])
-    download = next(x for x in latest if platform in x)
+    download = next(x for x in latest if target_platform in x)
     return download
 
 
 def launch_azbrowse():
-    azbrowse = os.path.join(get_config_dir(), 'azbrowse')
+    config_dir = get_config_dir()
+    executable = 'azbrowse'
+    
+    current_platform = platform.system()
+    if current_platform == 'Windows':
+        executable += '.exe'
+    
+    azbrowse = os.path.join(config_dir, executable)
 
     # azbrowse is self-updating
     if not os.path.isfile(azbrowse):
-        latest_azbrowse = get_latest_azbrowse()
+        latest_azbrowse = get_latest_azbrowse(current_platform)
         logger.warn('Downloading latest azbrowse from {}'.format(latest_azbrowse))
         r = requests.get(latest_azbrowse, allow_redirects=True, stream=True)
-        with open(azbrowse, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
+
+        if latest_azbrowse.endswith('.zip'):
+            with ZipFile(BytesIO(r.content)) as zipfile:
+                zipfile.extract(executable, path=config_dir)
+        elif latest_azbrowse.endswith('.tar.gz'):
+            with tarfile.open(fileobj=BytesIO(r.content), mode='r:gz') as tar:
+                tar.extract(executable, path=config_dir)
         os.chmod(azbrowse, 0o755)
 
     logger.warn('Launching azbrowse, hit `Ctrl+C` to close')
