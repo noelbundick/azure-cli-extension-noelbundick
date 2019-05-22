f12d10ab7fcab0e314d93069df9ce22ed2037b0e
Noel Bundick <noelbundick@gmail.com>
2019-05-22 16:07:41 -0700

    Fix azssh download for Windows

--- src/noelbundick/azext_noelbundick/cloudshell.py
+++ src/noelbundick/azext_noelbundick/cloudshell.py
@@ -1,6 +1,7 @@
 import json
 import operator
 import os
+import platform
 import re
 import requests
 import shutil
@@ -11,6 +12,7 @@ import tarfile
 from azure.cli.core.api import get_config_dir
 from azure.cli.core.commands import CliCommandType
 from azure.cli.core.commands.parameters import get_enum_type
+from io import BytesIO
 from knack.log import get_logger
 
 logger = get_logger(__name__)
@@ -28,23 +30,29 @@ def load_arguments(self, _):
         c.argument('shell', options_list=['--shell', '-s'], arg_type=get_enum_type(['bash', 'pwsh']))
 
 
-def get_latest_azssh():
-    platform = sys.platform
+def get_latest_azssh(current_platform):
+    target_platform = '{}-amd64'.format(current_platform.lower())
 
     r = requests.get('https://api.github.com/repos/noelbundick/azssh/releases')
     releases = r.json()
 
     releases = sorted(releases, key=lambda x: x['published_at'], reverse=True)
     latest = (x['browser_download_url'] for x in releases[0]['assets'])
-    download = next(x for x in latest if platform in x)
+    download = next(x for x in latest if target_platform in x)
     return download
 
 
 def launch_cloudshell(shell='bash'):
     is_current = False
+    config_dir = get_config_dir()
+    executable = 'azssh'
     
-    latest_azssh = get_latest_azssh()
-    azssh = os.path.join(get_config_dir(), 'azssh')
+    current_platform = platform.system()
+    if current_platform == 'Windows':
+        executable += '.exe'
+    azssh = os.path.join(config_dir, executable)
+
+    latest_azssh = get_latest_azssh(current_platform)
 
     if os.path.isfile(azssh):
         match = re.search('https://github.com/noelbundick/azssh/releases/download/v(?P<version>.*)/(.*).tar.gz', latest_azssh)
@@ -57,14 +65,9 @@ def launch_cloudshell(shell='bash'):
     
     if not is_current:
         logger.warn('Downloading latest azssh from {}'.format(latest_azssh))
-        tarball = os.path.join(get_config_dir(), 'azssh.tar.gz')
         r = requests.get(latest_azssh, allow_redirects=True, stream=True)
-        with open(tarball, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
-
-        logger.warn('Extracting tarball: {}'.format(tarball))
-        with tarfile.open(tarball) as f:
-            f.extractall(path=get_config_dir())
+        with tarfile.open(fileobj=BytesIO(r.content)) as tar:
+            tar.extract('./{}'.format(executable), path=config_dir)
 
     logger.warn('Launching Azure Cloud Shell, type `exit` to disconnect')
     os.system('{} -s {}'.format(azssh, shell))
