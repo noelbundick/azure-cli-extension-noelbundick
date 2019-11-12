c86a4d2e466b22899a13b0403deecaf0b61248f4
Noel Bundick <noelbundick@gmail.com>
2019-11-11 20:00:01 -0800

    Try VS Online devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
+apt-get install python3
+python3 -m venv .venv
+
+python -m pip install 'src/noelbundick[dev]'
+pip install --upgrade --target ~/.azure/devcliextensions/noelbundick src/noelbundick
